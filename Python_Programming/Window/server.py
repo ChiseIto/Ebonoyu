aa8582f72f385eb11b0d474eecc8a07f409f7759
ChiseIto <133559972+ChiseIto@users.noreply.github.com>
2024-01-11 16:57:52 +0900

    作業途中

---
+++ Python_Programming/Window/server.py
@@ -0,0 +1,32 @@
+import socket
+
+# ソケットオブジェクトの作成
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# IPとポートの設定
+server_socket.bind(('localhost', 9999))
+
+# 接続待機
+server_socket.listen(1)
+
+print('サーバーが起動しました。クライアントの接続を待っています...')
+
+# クライアントからの接続を受け入れる
+client_socket, address = server_socket.accept()
+
+print(f"{address} から接続がありました。")
+
+while True:
+    # メッセージの受信
+    msg = client_socket.recv(1024).decode('utf-8')
+    if msg == 'exit':
+        break
+    print(f"クライアント: {msg}")
+
+    # メッセージの送信
+    msg = input('サーバー: ')
+    client_socket.send(msg.encode('utf-8'))
+
+# ソケットのクローズ
+client_socket.close()
+server_socket.close()
